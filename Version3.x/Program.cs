b1bdea3ae2a86f307ce9940b41b45a854cd45b93
Cuong Vu <vucuongkg@gmaill.com>
2021-06-15 11:16:03 +0700

    Init

---
+++ Version3.x/Program.cs
@@ -0,0 +1,26 @@
+﻿using Lucene.Net.Analysis.Standard;
+using Lucene.Net.QueryParsers;
+using System;
+using Version = Lucene.Net.Util.Version;
+
+namespace Version3.x
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var searchPhrase = "Variant1 - Variant2";
+            var searchPhrase1 = "Variant1 + Variant2";
+
+            var parser = new QueryParser(Version.LUCENE_30, "content", new StandardAnalyzer(Version.LUCENE_30))
+            {
+                DefaultOperator = QueryParser.Operator.AND
+            };
+
+            Console.WriteLine(parser.Parse(searchPhrase).ToString());
+            Console.WriteLine(parser.Parse(searchPhrase1).ToString());
+
+            Console.Read();
+        }
+    }
+}
---
+++ Version4.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using Lucene.Net.Analysis.Standard;
+using Lucene.Net.QueryParsers.Classic;
+using Version = Lucene.Net.Util.LuceneVersion;
+using System;
+
+namespace Version4._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var searchPhrase1 = "Variant1 - Variant2";
+            var searchPhrase2 = "Variant1 + Variant2";
+
+            var parser = new QueryParser(Version.LUCENE_48, "_content", new StandardAnalyzer(Version.LUCENE_48))
+            {
+                DefaultOperator = QueryParserBase.AND_OPERATOR
+            };
+
+            Console.WriteLine(parser.Parse(searchPhrase1).ToString());
+            Console.WriteLine(parser.Parse(searchPhrase2).ToString());
+
+            Console.Read();
+        }
+    }
+}
